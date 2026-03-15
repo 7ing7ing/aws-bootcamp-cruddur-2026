84514e4c849b1e3d7ce7d5240b35e2527e11c048
Ting-Ting Chen <hchunyu.tty@gmail.com>
2026-03-15 15:06:15 +0000

    Finish implementing Rollbar

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -31,6 +31,12 @@ import watchtower
 import logging
 from time import strftime
 
+# Rollbar
+import os
+from flask import Flask, got_request_exception
+import rollbar
+import rollbar.contrib.flask
+
 # Configuring Logger to Use CloudWatch
 # LOGGER = logging.getLogger(__name__)
 # LOGGER.setLevel(logging.DEBUG)
@@ -94,6 +100,28 @@ def data_message_groups():
     return model['errors'], 422
   else:
     return model['data'], 200
+
+# Rollbar
+rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
+with app.app_context():
+    """init rollbar module"""
+    rollbar.init(
+        # access token
+        rollbar_access_token,
+        # environment name
+        'production',
+        # server root directory, makes tracebacks prettier
+        root=os.path.dirname(os.path.realpath(__file__)),
+        # flask already sets up logging
+        allow_logging_basic_config=False)
+
+    # send exceptions from `app` to rollbar, using flask's signal system.
+    got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
+
+@app.route('/rollbar/test')
+def rollbar_test():
+    rollbar.report_message('Hello World!', 'warning')
+    return "Hello World!"
 
 @app.route("/api/messages/@<string:handle>", methods=['GET'])
 def data_messages(handle):
